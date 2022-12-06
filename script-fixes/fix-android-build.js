764822f155a90d6d614671a90dbde1b9399c1b40
alex-moroz <foreinger13@gmail.com>
2022-12-06 11:31:37 +0200

    add script for automate fix android build

---
+++ script-fixes/fix-android-build.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+// HOOKS FOR FIX ANDROID BUILD
+// init base variables
+const fs = require('fs');
+const path = require('path');
+// get place of current script folder and remove it name to take a root one
+const rootdir = process.mainModule.path.replace('script-fixes', '');
+
+
+// This hook removes specific permissions from the AndroidManifest.xml
+// The AndroidManifest is re-generated during the prepare stage,
+// so this must be run on the "after_prepare" hook.
+function fixCapacitorCordovaPluginsManifest() {
+    // make sure that requested manifest file placed here
+    const manifestFile = path.join(rootdir, "android/capacitor-cordova-android-plugins/src/main/AndroidManifest.xml");
+
+    fs.readFile(manifestFile, "utf8", (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        let result = data.toString();
+        result = result.replace(`<uses-permission android:name="android.permission.REQUEST_INSTALL_PACKAGES"/>\n`, "");
+        result = result.replace(
+            `<service android:name="com.adobe.phonegap.push.FCMService">`,
+            `<service android:name="com.adobe.phonegap.push.FCMService" android:exported="true">`
+        );
+        result = result.replace(
+            `<service android:name="com.adobe.phonegap.push.PushInstanceIDListenerService">`,
+            `<service android:name="com.adobe.phonegap.push.PushInstanceIDListenerService" android:exported="true">`,
+        );
+
+
+        fs.writeFile(manifestFile, result, "utf8", onError);
+        console.log('REQUEST_INSTALL_PACKAGES: permissions was removed')
+    });
+
+}
+
+// This hook replace deprecated class to actual one
+// "Whitelist" should be change to "AllowList"
+function fixWhitelistUsage() {
+    const placesOfUsage = [
+        "android/capacitor-cordova-android-plugins/src/main/java/org/apache/cordova/whitelist/WhitelistPlugin.java",
+        "android/capacitor-cordova-android-plugins/src/main/java/org/apache/cordova/filetransfer/FileTransfer.java",
+    ];
+
+    placesOfUsage.forEach((src) => {
+        const sourceFile = path.join(rootdir, src);
+
+        fs.readFile(sourceFile, "utf8", (err, data) => {
+            if (err) {
+                return console.log(err);
+            }
+            let result = data.toString();
+            result = result.replaceAll('Whitelist', 'AllowList');
+            result = result.replaceAll('WhiteList', 'AllowList');
+            result = result.replaceAll('AllowListPlugin', 'WhitelistPlugin');
+
+            fs.writeFile(sourceFile, result, "utf8", onError);
+            console.log(src.split('/')[src.split('/').length - 1], '-- FIXED')
+        });
+    })
+
+}
+
+// error callback
+function onError(err) {
+    if (err) {
+        return console.log(err);
+    }
+}
+
+// LAUNCH FIX FUNCTIONS
+fixCapacitorCordovaPluginsManifest();
+fixWhitelistUsage();
